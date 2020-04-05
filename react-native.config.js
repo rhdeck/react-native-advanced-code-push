0db872d74bcc16720fe3a9ed331440c1e5b24b4d
Ray Deck <ray@raydeck.com>
2020-04-05 13:50:27 -0500

    Moving to advanced shape - includes both ios and android pieces

--- react-native.config.js
+++ react-native.config.js
@@ -1,15 +1,53 @@
 const xml2js = require("xml2js");
-const { readFileSync, writeFileSync } = require("fs");
 const { join } = require("path");
 const { sync: glob } = require("glob");
 const { spawnSync } = require("child_process");
+const { existsSync, readFileSync, writeFileSync } = require("fs");
+const { join } = require("path");
 module.exports = {
+  advanced: {
+    startupClasses: ["RNSCodePush.RNSCodePush"],
+    prelink: (path = process.cwd()) => {
+      //Update a couple of key files.
+      //Look for the
+      const sgPath = join(path, "android", "settings.gradle");
+      if (existsSync(sgPath)) {
+        const text = readFileSync(sgPath, { encoding: "utf8" });
+        const newText =
+          "project(':react-native-code-push').projectDir = new File(rootProject.projectDir, '../node_modules/react-native-code-push/android/app')";
+        if (!text.includes(newText)) {
+          writeFileSync(sgPath, [text, newText].join("\n"));
+        }
+      }
+      const bgPath = join(path, "android", "app", "build.gradle");
+      if (existsSync(bgPath)) {
+        const bgText = readFileSync(bgPath, { encoding: "utf8" });
+        const referenceText =
+          "\n" + 'apply from: "../../node_modules/react-native/react.gradle"';
+        const bgNewText =
+          'apply from: "../../node_modules/react-native-code-push/android/codepush.gradle"';
+        if (!bgText.includes(bgNewText)) {
+          writeFileSync(
+            bgPath,
+            bgText.replace(referenceText, [referenceText, bgNewText].join("\n"))
+          );
+        }
+      }
+    },
+  },
   commands: [
     {
-      name: "set-android-code-push-key [key]",
-      description:
-        "set codepush key for ios. Options only available if yarn is installed",
+      name: "set-code-push-key [key]",
+      description: "Set codepush key (platform specification required)",
       options: [
+        {
+          name: "--ios",
+          description: "Set for iOS project (this or --android is required",
+        },
+        {
+          name: "--android",
+          description: "Set for Android (this or --ios is required)",
+        },
         {
           name: "--appName [name]",
           description: "Name of application",
@@ -21,7 +59,13 @@ module.exports = {
           default: "",
         },
       ],
-      func: ([key], _, { appName, stage }) => {
+      func: ([key], _, { appName, stage, android, ios }) => {
+        if (!android && !ios) {
+          console.error(
+            "--android or --ios specification is required (set both to use same key on both"
+          );
+          process.exit(1);
+        }
         if (!key) {
           if (!appName) {
             //List appnames
@@ -39,7 +83,10 @@ module.exports = {
                 a
                   .map(
                     ({ name, owner: { name: ownerName } }) =>
-                      "\t react-native set-android-code-push-key --appName " +
+                      "\t react-native set-code-push-key " +
+                      (ios ? " --ios" : "") +
+                      (android ? " --ios" : "") +
+                      " --appName " +
                       [ownerName, name].join("/")
                   )
                   .join("\n")
@@ -88,10 +135,12 @@ module.exports = {
               a2
                 .map(
                   ([stage]) =>
-                    "\treact-native set-android-code-push-key --appName " +
+                    "\treact-native set-code-push-key --appName " +
                     appName +
                     " --stage " +
-                    stage
+                    stage +
+                    (ios ? " --ios" : "") +
+                    (android ? " --ios" : "")
                 )
                 .join("\n")
             );
@@ -102,35 +151,52 @@ module.exports = {
           }
         }
         if (key) {
-          const paths = glob(
-            join(
-              process.cwd(),
-              "android",
-              "app",
-              "src",
-              "main",
-              "**",
-              "strings.xml"
-            )
-          );
-          paths.forEach(async (path) => {
-            const xml = readFileSync(path, { encoding: "utf8" });
-
-            const o = await xml2js.parseStringPromise(xml);
-            //look for my keys
-            const strings = o.resources.string;
-            let myString = strings.find(
-              ({ $: { name } }) => name === "CodePushDeploymentKey"
+          if (android) {
+            const paths = glob(
+              join(
+                process.cwd(),
+                "android",
+                "app",
+                "src",
+                "main",
+                "**",
+                "strings.xml"
+              )
             );
-            if (!myString) {
-              strings.push({ $: { name: "CodePushDeploymentKey" }, _: key });
-            } else {
-              myString._ = key;
-            }
-            const out = new xml2js.Builder().buildObject(o);
-            writeFileSync(path, out);
-            console.log("Added deployment key", key, "to", path);
-          });
+            paths.forEach(async (path) => {
+              const xml = readFileSync(path, { encoding: "utf8" });
+
+              const o = await xml2js.parseStringPromise(xml);
+              //look for my keys
+              const strings = o.resources.string;
+              let myString = strings.find(
+                ({ $: { name } }) => name === "CodePushDeploymentKey"
+              );
+              if (!myString) {
+                strings.push({ $: { name: "CodePushDeploymentKey" }, _: key });
+              } else {
+                myString._ = key;
+              }
+              const out = new xml2js.Builder().buildObject(o);
+              writeFileSync(path, out);
+              console.log("Added deployment key", key, "to", path);
+            });
+          }
+          if (ios) {
+            const paths = glob(
+              join(process.cwd(), "ios", "*", "Info.plist")
+            ).filter(
+              (path) => !path.includes("/Pods/") && !path.includes("-tvOS")
+            );
+            paths.forEach((path) => {
+              const xml = readFileSync(path, { encoding: "utf8" });
+              const o = Plist.parse(xml);
+              o.CodePushDeploymentKey = key;
+              const out = Plist.build(o);
+              writeFileSync(path, out);
+              console.log("Added deployment key", key, "to", path);
+            });
+          }
         }
       },
     },
